05ad217b2f608e00bd2c82f5d04468d315b3b144
maniek <mariusz@gmail.com>
2020-05-31 13:00:35 +0200

    [fix] - fixing already closed connection

--- src/test/java/pl/mpas/jdbc_course/dao/PersonDaoTest.java
+++ src/test/java/pl/mpas/jdbc_course/dao/PersonDaoTest.java
@@ -42,29 +42,23 @@ public class PersonDaoTest {
 
     @Test
     public void readAllPersonsFromDbTest() {
-        try (Connection connection = DbConnectionConfig.getInstance().getConnection()) {
-            PersonDao personDao = new PersonDaoImpl(connection);
+        Connection connection = DbConnectionConfig.getInstance().getConnection();
+        PersonDao personDao = new PersonDaoImpl(connection);
 
-            List<Person> peopleFromDb = personDao.readAllPersons();
-            Assert.assertTrue("Didn't find any people inside db!", peopleFromDb.size() > 0);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        List<Person> peopleFromDb = personDao.readAllPersons();
+        Assert.assertTrue("Didn't find any people inside db!", peopleFromDb.size() > 0);
     }
 
     @Test
     public void readOnlyAdultTest() {
-        try (Connection connection = DbConnectionConfig.getInstance().getConnection()) {
-            PersonDao personDao = new PersonDaoImpl(connection);
+        Connection connection = DbConnectionConfig.getInstance().getConnection();
+        PersonDao personDao = new PersonDaoImpl(connection);
 
-            List<Person> peopleFromDb = personDao.readOnlyAdult();
-            Assert.assertTrue("Didn't find any adult inside db!", peopleFromDb.size() > 0);
+        List<Person> peopleFromDb = personDao.readOnlyAdult();
+        Assert.assertTrue("Didn't find any adult inside db!", peopleFromDb.size() > 0);
 
-            for (Person p : peopleFromDb) {
-                Assert.assertTrue("This is not adult: " + p, p.getAge() >= 18);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        for (Person p : peopleFromDb) {
+            Assert.assertTrue("This is not adult: " + p, p.getAge() >= 18);
         }
     }
 
@@ -121,29 +115,21 @@ public class PersonDaoTest {
 //                }
 //            }
 //        }
-        try (Connection connection = DbConnectionConfig.getInstance().getConnection()) {
-            PersonDao personDao = new PersonDaoImpl(connection);
+        Connection connection = DbConnectionConfig.getInstance().getConnection();
+        PersonDao personDao = new PersonDaoImpl(connection);
 
-            List<Person> peopleFromDbSoFar = personDao.readAllPersons();
-
-            personDao.savePerson(somebodyToSave);
-            Assert.assertTrue("Person wasn't save to db", somebodyToSave.getId() != Person.ID_OF_NOT_PERSISTENT_PERSON);
-            // TODO:MP get all persons again and compare if returned list contains new added person
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        personDao.savePerson(somebodyToSave);
+        Assert.assertTrue("Person wasn't save to db", somebodyToSave.getId() != Person.ID_OF_NOT_PERSISTENT_PERSON);
+        // TODO:MP get all persons again and compare if returned list contains new added person
     }
 
     @Test
     public void checkPersonByName() {
-        try (Connection connection = DbConnectionConfig.getInstance().getConnection()) {
-            PersonDao personDao = new PersonDaoImpl(connection);
+        Connection connection = DbConnectionConfig.getInstance().getConnection();
+        PersonDao personDao = new PersonDaoImpl(connection);
 
-            List<Person> persons = personDao.findBySurname("L.");
-            System.out.println("found persons: " + persons);
-            Assert.assertEquals("wrong data found!!!", 2, persons.size());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        List<Person> persons = personDao.findBySurname("L.");
+        System.out.println("found persons: " + persons);
+        Assert.assertEquals("wrong data found!!!", 2, persons.size());
     }
 }
